cf0df79d92e07034b62b11dcff20f54f5b454b31
karimi254 <kariminic@gmail.com>
2019-10-04 19:26:50 +0300

    modified the password manager to remove personal details

--- Dictionaries/passwdManager.py
+++ Dictionaries/passwdManager.py
@@ -2,16 +2,10 @@
 
 """ passwdManager.py - not so secure program that stores account pass"""
 
-PASSWORDS = {'email': 'P9MYTr45EWQmn08L',
-       	 'AROMA CAFE': '#password',
-	 'linux-user': 'Khsng5Gv',
-	 'edx': '$karimi95$',
-         'IMEI': 356023081942718 / 68,
-         'SerialNo': 'D1AGAS3770137210',
-         'wp' : 'virgine2019',
-         'USSDIMEI': '*#06#',
-         'nicholas.muigai': '$nikki95$',
-         'Swahilipot Hub': '5PHm0mb@s@'
+PASSWORDS = {'email': '',
+       	 'AROMA CAFE': '',
+	 'linux-user': '',
+	 'Swahilipot Hub': ''
          }
 
 import sys, pyperclip
